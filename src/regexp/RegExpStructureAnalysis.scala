14af1e6c9eb442aea9393061c62a6237e9c36fa3
Futa Miyachi <git@jj1uzh.net>
2023-04-24 10:45:08 +0900

    b

--- src/regexp/RegExpStructureAnalysis.scala
+++ src/regexp/RegExpStructureAnalysis.scala
@@ -1,6 +1,7 @@
 package matching.regexp
 
 import scala.collection.mutable.ArrayBuffer
+import scala.io.AnsiColor._
 
 object RegExpStructureAnalysis {
 
@@ -32,14 +33,27 @@ object RegExpStructureAnalysis {
   final object HasLookbehindWithCapture extends StructureAnalyzer[Char, RegExp[Char]] {
     def name = "Has backreferenced captures inside lookbehinds?"
     def analyze(r: RegExp[Char]): Option[RegExp[Char]] = {
-      captInLB(r).find(captIsBackrefed(r, _))
+      (for (
+        lb <- listLB(r);
+        capt <- listCapt(lb);
+        if captIsBackrefed(r, capt)
+      ) yield capt).headOption
     }
-    private def captInLB(r: RegExp[Char]): LazyList[GroupExp[Char]] = {
+    private def listLB(r: RegExp[Char]): LazyList[LookbehindExp[Char]] = {
       r match {
-        case exp @ GroupExp(r, _, _) => exp #:: captInLB(r)
-        case RegExpOp1(r1)           => captInLB(r1)
-        case RegExpOp2(r1, r2)       => captInLB(r1) ++ captInLB(r2)
-        case RegExpOp3(r1, r2, r3)   => captInLB(r1) ++ captInLB(r2) ++ captInLB(r3)
+        case exp @ LookbehindExp(r, _) => exp #:: listLB(r)
+        case RegExpOp1(r1)             => listLB(r1)
+        case RegExpOp2(r1, r2)         => listLB(r1) ++ listLB(r2)
+        case RegExpOp3(r1, r2, r3)     => listLB(r1) ++ listLB(r2) ++ listLB(r3)
+        case _                         => LazyList.empty
+      }
+    }
+    private def listCapt(r: RegExp[Char]): LazyList[GroupExp[Char]] = {
+      r match {
+        case exp @ GroupExp(r, _, _) => exp #:: listCapt(r)
+        case RegExpOp1(r1)           => listCapt(r1)
+        case RegExpOp2(r1, r2)       => listCapt(r1) ++ listCapt(r2)
+        case RegExpOp3(r1, r2, r3)   => listCapt(r1) ++ listCapt(r2) ++ listCapt(r3)
         case _                       => LazyList.empty
       }
     }
@@ -55,6 +69,13 @@ object RegExpStructureAnalysis {
     }
   }
 
+  final case class MarkedRegExp[A](child: RegExp[A]) extends RegExp[A] {
+    override def toString(): String = s"${UNDERLINED}${child}${RESET}"
+  }
+  private def marked[A](r: RegExp[A], sub: RegExp[A]): RegExp[A] = {
+    if (r == sub) MarkedRegExp(r) else recursiveApply(r, marked(_, sub))
+  }
+
   final class StructureAnalyzersManager(
       analyzers: Seq[StructureAnalyzer[Char, RegExp[Char]]]
   ) {
@@ -65,7 +86,9 @@ object RegExpStructureAnalysis {
     def analyze(r: RegExp[Char]): Unit = {
       total += 1
       zipped.foreach { case (analyzer, set) =>
-        analyzer.analyze(r).foreach { subexp => set += r }
+        analyzer.analyze(r).foreach { subexp =>
+          set += marked(r, subexp)
+        }
       }
     }
     def printResult(): Unit = {
@@ -79,5 +102,30 @@ object RegExpStructureAnalysis {
   object StructureAnalyzersManager {
     def apply(analyzers: StructureAnalyzer[Char, RegExp[Char]]*): StructureAnalyzersManager =
       new StructureAnalyzersManager(analyzers)
+  }
+
+  // TODO:tmp
+  private def recursiveApply[A](r: RegExp[A], f: RegExp[A] => RegExp[A]): RegExp[A] = {
+    def optConcatExp[A](r1: RegExp[A], r2: RegExp[A]): RegExp[A] = {
+      (r1, r2) match {
+        case (EpsExp(), _) => r2
+        case (_, EpsExp()) => r1
+        case (_, RepeatExp(`r1`, min, max, greedy)) =>
+          RepeatExp(r1, min.orElse(Some(0)).map(_ + 1), max.map(_ + 1), greedy)
+        case _ => ConcatExp(r1, r2)
+      }
+    }
+    r match {
+      case ConcatExp(r1, r2)              => optConcatExp(f(r1), f(r2))
+      case AltExp(r1, r2)                 => AltExp(f(r1), f(r2))
+      case StarExp(r, greedy)             => StarExp(f(r), greedy)
+      case PlusExp(r, greedy)             => PlusExp(f(r), greedy)
+      case OptionExp(r, greedy)           => OptionExp(f(r), greedy)
+      case RepeatExp(r, min, max, greedy) => RepeatExp(f(r), min, max, greedy)
+      case GroupExp(r, id, name)          => GroupExp(f(r), id, name)
+      case LookaheadExp(r, positive)      => LookaheadExp(f(r), positive)
+      case LookbehindExp(r, positive)     => LookbehindExp(f(r), positive)
+      case _                              => r
+    }
   }
 }
